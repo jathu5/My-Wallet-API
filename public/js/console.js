c9a9e683b95fb841b20baf39882c4df2999d0d09
jathu-git <jathu.theiv@gmail.com>
2021-05-07 16:20:02 -0400

    add information

--- public/js/console.js
+++ public/js/console.js
@@ -1,5 +1,6 @@
 import { changeAccounts } from './helpers/console/changeAccounts.js';
 import { consoleInputs } from './helpers/console/consoleInputs.js';
+import { other } from './helpers/console/minorDetails.js';
 import { updateDisplay } from './helpers/console/updateDisplay.js';
 
 const home = document.getElementById('home');
@@ -8,37 +9,6 @@ let user;
 home.addEventListener('click', () => {
     window.location.href = '/home';
 });
-
-const help = () => {
-    const dropdown = document.getElementById('dropdown');
-    const dropdownContent = document.getElementById('dropdown-content');
-    const dropdownContain = document.getElementById('all-dropdown');
-    const blocks = document.querySelectorAll('.block');
-
-    dropdown.addEventListener('mouseover', () => {
-        dropdownContent.style.display = 'block';
-    });
-
-    dropdownContain.addEventListener('mouseleave', () => {
-        dropdownContent.style.display = 'none';
-    });
-
-    const messages = ['[positive number] [account code]', '[non-zero number] [account code]', 'Click on add icon in accuont slot and follow',
-        '[negative number] [account code]', 'rep', 'Click on account icon and follow',
-        '[non-zero number] [deposit account code] [withdrawl account code]', '- rep', 'Click on account icon and follow'];
-    for (let i = 0; i < blocks.length; ++i) {
-        blocks[i].addEventListener('mouseenter', () => {
-            blocks[i].textContent = messages[i];
-        });
-
-        const initialBlocks = ['Regular Deposit', 'Absolute Deposit', 'Add Account',
-            'Withdrawl', 'Repeat Previous', 'Change Account',
-            'Transfer', 'Cancel Previous', 'Delete Account'];
-        blocks[i].addEventListener('mouseout', () => {
-            blocks[i].textContent = initialBlocks[i];
-        });
-    }
-}
 
 const userSigned = user => {
     if (!JSON.parse(sessionStorage.getItem('signed'))) {
@@ -51,7 +21,7 @@ const userSigned = user => {
     sessionStorage.setItem('addStatus', undefined);
     consoleInputs(user);
     changeAccounts(user);
-    help();
+    other();
 
     let executed = false;
     if (!executed) {
@@ -68,5 +38,5 @@ fetch('user')
         }
     }).catch(err => {
         console.log(err);
-        window.location.href = '/home'
+     //   window.location.href = '/home'
     });
---
+++ public/js/helpers/console/minorDetails.js
@@ -0,0 +1,52 @@
+export const other = () => {
+    helpFunc();
+    instructionsFunc();
+}
+
+const helpFunc = () => {
+    const dropdown = document.getElementById('dropdown');
+    const dropdownContent = document.getElementById('dropdown-content');
+    const dropdownContain = document.getElementById('all-dropdown');
+    const blocks = document.querySelectorAll('.block');
+
+    dropdown.addEventListener('mouseover', () => {
+        dropdownContent.style.display = 'block';
+    });
+
+    dropdownContain.addEventListener('mouseleave', () => {
+        dropdownContent.style.display = 'none';
+    });
+
+    const messages = ['[positive number] [account code]', '[non-zero number] [account code]', 'Click on add icon in accuont slot and follow',
+        '[negative number] [account code]', 'rep', 'Click on account icon and follow',
+        '[non-zero number] [deposit account code] [withdrawl account code]', '- rep', 'Click on account icon and follow'];
+    for (let i = 0; i < blocks.length; ++i) {
+        blocks[i].addEventListener('mouseenter', () => {
+            blocks[i].textContent = messages[i];
+        });
+
+        const initialBlocks = ['Regular Deposit', 'Absolute Deposit', 'Add Account',
+            'Withdrawl', 'Repeat Previous', 'Change Account',
+            'Transfer', 'Cancel Previous', 'Delete Account'];
+        blocks[i].addEventListener('mouseout', () => {
+            blocks[i].textContent = initialBlocks[i];
+        });
+    }
+}
+
+
+export function instructionsFunc() {
+    const information = document.getElementById('information');
+    const visibleImage = document.querySelectorAll('.visible-image');
+    information.addEventListener('mouseover', () => {
+        visibleImage.forEach(img => {
+            img.style.display = 'inline-block';
+        })
+    })
+
+    information.addEventListener('mouseout', () => {
+        visibleImage.forEach(img => {
+            img.style.display = 'none';
+        })
+    })
+}
