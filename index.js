bd9d0baea663552274f5a1300e12911ee2b3f8d2
Jath <theivikaran.jathurshan@gmail.com>
2021-10-23 01:05:29 -0400

    attempt fix

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
 // import packages
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // global constants
 const port = 5000;
 
 // middlewares to access client-side javascript
-app.use(express.static('public'));
-app.use('/', express.static(__dirname + 'public/css'));
+app.use(express.static("public"));
+app.use("/", express.static(__dirname + "public/css"));
 
 // parse all body requests
 app.use(express.json());
@@ -20,30 +19,34 @@ app.use(express.json());
 app.use(cookieParser());
 
 // middleware to handle all required requests
-app.use('/user', require('./routes/otherRequests.js'));
-app.use('/user', require('./routes/postRequests.js'));
+app.use("/user", require("./routes/otherRequests.js"));
+app.use("/user", require("./routes/postRequests.js"));
 
 // connect html files to client-side javascript
-const connectPages = path => {
-    app.get(path, (req, res) => {
-        if (path === '') {
-            path = '/home';
-        }
-        res.sendFile(`${__dirname}/views${path}.html`);
-    });
-}
+const connectPages = (path) => {
+  app.get(path, (req, res) => {
+    if (path === "") {
+      path = "/home";
+    }
+    res.sendFile(`${__dirname}/views${path}.html`);
+  });
+};
 
-connectPages('');
-connectPages('/signin');
-connectPages('/signup');
-connectPages('/console');
+connectPages("");
+connectPages("/signin");
+connectPages("/signup");
+connectPages("/console");
 
 // connect to database
-mongoose.connect(process.env.databaseURL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('database connected [ignore]')
-});
+mongoose.connect(
+  "mongodb+srv://user:password101@notable.uvjod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("database connected [ignore]");
+  }
+);
 
 // boot server
 app.listen(process.env.PORT || port, () => {
-    console.log('\nserver running [ignore]')
+  console.log("\nserver running [ignore]");
 });
--- validation/verifyToken.js
+++ validation/verifyToken.js
@@ -1,21 +1,24 @@
 // import packages and another module with common functions
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // verify if user is signed in
 const verify = (req, res, next) => {
-    const token = req.cookies.authToken;
-    if (!token) {
-        res.status(401).send({ error: 'Access Denied' });
-    } else {
-        try {
-            const verified = jwt.verify(token, process.env.tokenSecret);
-            req.user = verified;
-            next();
-        } catch (err) {
-            res.status(400).send(err);
-        }
+  const token = req.cookies.authToken;
+  if (!token) {
+    res.status(401).send({ error: "Access Denied" });
+  } else {
+    try {
+      const verified = jwt.verify(
+        token,
+        "WBTSoG==%5nMIWbfGW0BB#=@ehpi$64Z*lKVZ*+Wnsr*4aJpVW"
+      );
+      req.user = verified;
+      next();
+    } catch (err) {
+      res.status(400).send(err);
     }
-}
+  }
+};
 
 // export function
 module.exports = verify;
