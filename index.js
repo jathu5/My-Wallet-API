ad529539d024e5836df7c71c2fea7d4a8e6675b8
Jath <61468377+jath-git@users.noreply.github.com>
2022-12-20 22:16:46 -0500

    update port

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 require("dotenv").config();
 
 // global constants
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 8000;
+const port = 8000;
 
 // middlewares to access client-side javascript
 app.use(express.static("public"));
